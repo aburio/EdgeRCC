06e96ad48b33127d4779041f7f789072ff114408
Arthur Burnichon <arthur@abur.io>
2021-12-09 22:51:10 +0100

    🚂 Motor control using asyncwebserver & websocket

--- src/server/server.cpp
+++ src/server/server.cpp
@@ -1,180 +1,42 @@
 #include "server.h"
 
+#include <ArduinoJson.h>
+#include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>
 #include <ESPmDNS.h>
-
 #if FILESYSTEM == SPIFFS
 #include <SPIFFS.h>
 #endif
-
 #include <Update.h>
-#include <WebServer.h>
 
 /* local types & variables */
-WebServer server(80);
+AsyncWebServer server(80);
+AsyncWebSocket socket("/ws");
+bool reboot = false;
 
 /* private functions prototypes */
-String formatBytes(size_t bytes);
-String getContentType(String filename);
-bool handleFileRead(String path);
+void request_file_upload_start(AsyncWebServerRequest *request, const String filename, size_t index, uint8_t *data, size_t len, bool final);
+void request_file_upload_end(AsyncWebServerRequest *request);
+void request_not_found(AsyncWebServerRequest *request);
+void socket_event(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type, void *arg, uint8_t *data, size_t len);
+void socket_message_parser(String message);
 
 /* public functions */
 void server_init(const String local_hostname, bool OTA)
 {
-  MDNS.begin(local_hostname.c_str());
-
+  // Filesystem
   FILESYSTEM.begin();
-  {
-    File root = FILESYSTEM.open("/");
-    File file = root.openNextFile();
-    while (file)
-    {
-      String fileName = file.name();
-      size_t fileSize = file.size();
-      log_v("FS File: %s, size: %s\n", fileName.c_str(), formatBytes(fileSize).c_str());
-      file = root.openNextFile();
-    }
-  }
 
-  server.onNotFound([]()
-                    {
-                      if (!handleFileRead(server.uri()))
-                      {
-                        server.send(404, "text/plain", "FileNotFound");
-                      }
-                    });
-
-  server.on("/function", []()
-            {
-              String param_name = server.arg("param");
-              String param_value = server.arg("value");
-
-              if (server.method() == HTTP_POST && param_name == "front-cabine")
-              {
-                if (param_value == "on")
-                {
-                  server.sendHeader("Connection", "close");
-                  server.send(200, "text/plain", "Success");
-                  return;
-                }
-                else if (param_value == "off")
-                {
-                  server.sendHeader("Connection", "close");
-                  server.send(200, "text/plain", "Success");
-                  return;
-                }
-
-                server.sendHeader("Connection", "close");
-                server.send(400, "text/plain", "Bad request !");
-              }
-              else
-              {
-                server.sendHeader("Connection", "close");
-                server.send(200, "text/plain", "GPIO X value : digitalRead(X)");
-              }
-            });
-
-  server.on("/motor", []()
-            {
-              String param_name = server.arg("param");
-              String param_value = server.arg("value");
-
-              if (server.method() == HTTP_POST)
-              {
-                if (param_name == "direction")
-                {
-                  if (param_value == "forward")
-                  {
-                    digitalWrite(INPUT1_PIN, HIGH);
-                    digitalWrite(INPUT2_PIN, LOW);
-                    server.sendHeader("Connection", "close");
-                    server.send(200, "text/plain", "Success");
-                    return;
-                  }
-                  else if (param_value == "stop")
-                  {
-                    digitalWrite(INPUT1_PIN, LOW);
-                    digitalWrite(INPUT2_PIN, LOW);
-                    server.sendHeader("Connection", "close");
-                    server.send(200, "text/plain", "Success");
-                    return;
-                  }
-                  else if (param_value == "backward")
-                  {
-                    digitalWrite(INPUT1_PIN, LOW);
-                    digitalWrite(INPUT2_PIN, HIGH);
-                    server.sendHeader("Connection", "close");
-                    server.send(200, "text/plain", "Success");
-                    return; 
-                  }
-                }
-                else if (param_name == "traction")
-                {
-                  ledcWrite(0, param_value.toInt());
-                  server.sendHeader("Connection", "close");
-                  server.send(200, "text/plain", "Success");
-                  return;
-                }
-
-                server.sendHeader("Connection", "close");
-                server.send(400, "text/plain", "Bad request !");
-              }
-            });
-
-  server.on(
-      "/update", HTTP_POST,
-      []()
-      {
-        // TODO : Remove reboot when uploading filesystem ?
-        server.sendHeader("Connection", "close");
-        server.send(200, "text/plain", (Update.hasError()) ? "FAIL" : "OK");
-        ESP.restart();
-      },
-      []()
-      {
-        HTTPUpload &upload = server.upload();
-        uint8_t cmd = U_FLASH;
-
-        if (upload.status == UPLOAD_FILE_START)
-        {
-          log_v("Update: %s\n", upload.filename.c_str());
-          if (upload.filename == "filesystem.bin")
-          {
-            cmd = U_SPIFFS;
-          }
-
-          if (!Update.begin(UPDATE_SIZE_UNKNOWN, cmd))
-          { //start with max available size
-            log_v("%s", Update.errorString());
-            Update.end(false);
-          }
-        }
-        else if (upload.status == UPLOAD_FILE_WRITE)
-        {
-          if (Update.write(upload.buf, upload.currentSize) != upload.currentSize)
-          {
-            log_v("%s", Update.errorString());
-            Update.end(false);
-          }
-        }
-        else if (upload.status == UPLOAD_FILE_END)
-        {
-          if (Update.end(true))
-          { //true to set the size to the current progress
-            log_v("Update Success: %u\nRebooting...\n", upload.totalSize);
-          }
-          else
-          {
-            log_v("%s", Update.errorString());
-            Update.end(false);
-          }
-        }
-        else
-        {
-          log_v("Update Failed Unexpectedly (likely broken connection): status=%d\n", upload.status);
-        }
-      });
-
+  // Server
+  socket.onEvent(socket_event);
+  server.addHandler(&socket);
+  server.serveStatic("/", SPIFFS, "/").setDefaultFile("index.html");
+  server.onNotFound(request_not_found);
+  server.on("/update", HTTP_POST, request_file_upload_end, request_file_upload_start);
   server.begin();
+
+  // MDNS
+  MDNS.begin(local_hostname.c_str());
   MDNS.addService("http", "tcp", 80);
 }
 
@@ -182,94 +44,209 @@ void server_task(void *pvParameters)
 {
   for (;;)
   {
-    server.handleClient();
-    vTaskDelay(50 / portTICK_PERIOD_MS);
+    socket.cleanupClients();
+
+    if (reboot == true)
+    {
+      vTaskDelay(5000 / portTICK_PERIOD_MS);
+      ESP.restart();
+    }
+
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
   }
 
   vTaskDelete(NULL);
 }
 
 /* private functions */
-String formatBytes(size_t bytes)
+void request_file_upload_start(AsyncWebServerRequest *request, const String filename, size_t index, uint8_t *data, size_t len, bool final)
 {
-  if (bytes < 1024)
+  uint8_t cmd = U_FLASH;
+
+  if (index == 0)
   {
-    return String(bytes) + "B";
+    log_v("Update: %s\n", filename.c_str());
+    if (filename == "filesystem.bin")
+    {
+      cmd = U_SPIFFS;
+    }
+
+    if (!Update.begin(UPDATE_SIZE_UNKNOWN, cmd))
+    {
+      log_v("%s", Update.errorString());
+      Update.end(false);
+      request->send(503, "text/plain", "Internal error !");
+    }
   }
-  else if (bytes < (1024 * 1024))
+
+  if (!Update.hasError())
   {
-    return String(bytes / 1024.0) + "KB";
+    if (Update.write(data, len) != len)
+    {
+      log_v("%s", Update.errorString());
+      Update.end(false);
+      request->send(503, "text/plain", "Internal error : " + String(Update.errorString()));
+    }
+    else
+    {
+      socket.printfAll("%zu", Update.progress());
+    }
   }
-  else if (bytes < (1024 * 1024 * 1024))
+
+  if (final)
   {
-    return String(bytes / 1024.0 / 1024.0) + "MB";
-  }
-  else
-  {
-    return String(bytes / 1024.0 / 1024.0 / 1024.0) + "GB";
+    if (Update.end(true))
+    {
+      log_v("Update Success: %uB\n", index + len);
+      AsyncWebServerResponse *response = request->beginResponse(302, "text/plain", "Please wait while the device reboots");
+      response->addHeader("Refresh", "60");
+      response->addHeader("Location", "/");
+      request->send(response);
+    }
+    else
+    {
+      log_v("%s", Update.errorString());
+      Update.end(false);
+      request->send(503, "text/plain", "Internal error : " + String(Update.errorString()));
+    }
   }
 }
 
-String getContentType(String filename)
+void request_file_upload_end(AsyncWebServerRequest *request)
 {
-  if (filename.endsWith(".htm"))
-  {
-    return "text/html";
-  }
-  else if (filename.endsWith(".html"))
-  {
-    return "text/html";
-  }
-  else if (filename.endsWith(".css"))
-  {
-    return "text/css";
-  }
-  else if (filename.endsWith(".js"))
-  {
-    return "application/javascript";
-  }
-  else if (filename.endsWith(".png"))
-  {
-    return "image/png";
-  }
-  else if (filename.endsWith(".gif"))
-  {
-    return "image/gif";
-  }
-  else if (filename.endsWith(".jpg"))
-  {
-    return "image/jpeg";
-  }
-  else if (filename.endsWith(".ico"))
-  {
-    return "image/x-icon";
-  }
-  else if (filename.endsWith(".xml"))
-  {
-    return "text/xml";
-  }
-  else if (filename.endsWith(".json"))
-  {
-    return "text/json";
-  }
-
-  return "text/plain";
+  reboot = !Update.hasError();
+  AsyncWebServerResponse *response = request->beginResponse(302, "text/plain", "Please wait while the device reboots");
+  response->addHeader("Refresh", "60");
+  response->addHeader("Location", "/");
+  request->send(response);
 }
 
-bool handleFileRead(String path)
+void request_not_found(AsyncWebServerRequest *request)
 {
-  log_v("handleFileRead: %s", path.c_str());
-  if (path.endsWith("/"))
+  request->send(404, "text/plain", "FileNotFound");
+}
+
+void socket_event(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type, void *arg, uint8_t *data, size_t len)
+{
+  if (type == WS_EVT_CONNECT)
   {
-    path += "index.html";
+    log_v("ws[%s][%u] connect\n", server->url(), client->id());
+    client->printf("Hello Client %u :)", client->id());
+    client->ping();
   }
-  String contentType = getContentType(path);
-  if (FILESYSTEM.exists(path))
+  else if (type == WS_EVT_DISCONNECT)
   {
-    File file = FILESYSTEM.open(path, "r");
-    server.streamFile(file, contentType);
-    file.close();
-    return true;
+    log_v("ws[%s][%u] disconnect\n", server->url(), client->id());
   }
-  return false;
+  else if (type == WS_EVT_ERROR)
+  {
+    log_v("ws[%s][%u] error(%u): %s\n", server->url(), client->id(), *((uint16_t *)arg), (char *)data);
+  }
+  else if (type == WS_EVT_PONG)
+  {
+    log_v("ws[%s][%u] pong[%u]: %s\n", server->url(), client->id(), len, (len) ? (char *)data : "");
+  }
+  else if (type == WS_EVT_DATA)
+  {
+    AwsFrameInfo *info = (AwsFrameInfo *)arg;
+    String msg = "";
+
+    if (info->final && info->index == 0 && info->len == len)
+    {
+      // the whole message is in a single frame and we got all of it's data
+      log_v("ws[%s][%u] %s-message[%llu]: ", server->url(), client->id(), (info->opcode == WS_TEXT) ? "text" : "binary", info->len);
+
+      if (info->opcode == WS_TEXT)
+      {
+        for (size_t i = 0; i < info->len; i++)
+        {
+          msg += (char)data[i];
+        }
+      }
+      else
+      {
+        char buff[3];
+        for (size_t i = 0; i < info->len; i++)
+        {
+          sprintf(buff, "%02x ", (uint8_t)data[i]);
+          msg += buff;
+        }
+      }
+      socket_message_parser(msg);
+      client->text(msg);
+    }
+    else
+    {
+      // message is comprised of multiple frames or the frame is split into multiple packets
+      if (info->index == 0)
+      {
+        if (info->num == 0)
+          log_v("ws[%s][%u] %s-message start\n", server->url(), client->id(), (info->message_opcode == WS_TEXT) ? "text" : "binary");
+        log_v("ws[%s][%u] frame[%u] start[%llu]\n", server->url(), client->id(), info->num, info->len);
+      }
+
+      log_v("ws[%s][%u] frame[%u] %s[%llu - %llu]: ", server->url(), client->id(), info->num, (info->message_opcode == WS_TEXT) ? "text" : "binary", info->index, info->index + len);
+
+      if (info->opcode == WS_TEXT)
+      {
+        for (size_t i = 0; i < len; i++)
+        {
+          msg += (char)data[i];
+        }
+      }
+      else
+      {
+        char buff[3];
+        for (size_t i = 0; i < len; i++)
+        {
+          sprintf(buff, "%02x ", (uint8_t)data[i]);
+          msg += buff;
+        }
+      }
+      log_v("%s\n", msg.c_str());
+
+      if ((info->index + len) == info->len)
+      {
+        log_v("ws[%s][%u] frame[%u] end[%llu]\n", server->url(), client->id(), info->num, info->len);
+        if (info->final)
+        {
+          log_v("ws[%s][%u] %s-message end\n", server->url(), client->id(), (info->message_opcode == WS_TEXT) ? "text" : "binary");
+          client->text("I got your text message");
+        }
+      }
+    }
+  }
+}
+
+void socket_message_parser(String message)
+{
+  StaticJsonDocument<256> json;
+  deserializeJson(json, message);
+  JsonObject obj = json.as<JsonObject>();
+
+  if (obj.getMember("function") != NULL)
+  {
+    if (obj.getMember("function") == "direction")
+    {
+      if (obj.getMember("value") == 1)
+      {
+        digitalWrite(INPUT1_PIN, HIGH);
+        digitalWrite(INPUT2_PIN, LOW);
+      }
+      else if (obj.getMember("value") == -1)
+      {
+        digitalWrite(INPUT1_PIN, LOW);
+        digitalWrite(INPUT2_PIN, HIGH);
+      }
+      else
+      {
+        digitalWrite(INPUT1_PIN, LOW);
+        digitalWrite(INPUT2_PIN, LOW);
+      }
+    }
+    else if (obj.getMember("function") == "traction")
+    {
+      ledcWrite(0, obj.getMember("value"));
+    }
+  }
 }
